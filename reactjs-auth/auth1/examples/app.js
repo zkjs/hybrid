a41baa93941237340f70ec1d2cdbf10422d7c62b
ruruyin <ruruyin@163.com>
2016-06-20 20:37:52 +0800

    react auth client

---
+++ reactjs-auth/auth1/examples/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { browserHistory, Router } from 'react-router'
+import routes from './auth-with-shared-root/config/routes.js'
+
+render(<Router history={browserHistory} routes={routes}/>, document.getElementById('example'))
---
+++ reactjs-auth/auth1/examples/auth-with-shared-root/utils/auth.js
@@ -0,0 +1,78 @@
+module.exports = {
+  login(email, pass, cb) {
+    cb = arguments[arguments.length - 1]
+    if (localStorage.token) {
+      if (cb) cb(true)
+      this.onChange(true)
+      return
+    }
+ realRequest(email, pass, (res) => {
+      if (res.authenticated) {
+        localStorage.token = res.token
+        if (cb) cb(true)
+        this.onChange(true)
+      } else {
+        if (cb) cb(false)
+        this.onChange(false)
+      }
+    })
+  },
+  /* pretendRequest(email, pass, (res) => {
+      if (res.authenticated) {
+        localStorage.token = res.token
+        if (cb) cb(true)
+        this.onChange(true)
+      } else {
+        if (cb) cb(false)
+        this.onChange(false)
+      }
+    })
+  },*/
+
+  getToken: function () {
+    return localStorage.token
+  },
+
+  logout: function (cb) {
+    delete localStorage.token
+    if (cb) cb()
+    this.onChange(false)
+  },
+
+  loggedIn: function () {
+    return !!localStorage.token
+  },
+
+  onChange: function () {}
+}
+/*
+function pretendRequest(email, pass, cb) {
+  setTimeout(() => {
+    if (email === 'joe@example.com' && pass === 'password1') {
+      cb({
+        authenticated: true,
+        token: Math.random().toString(36).substring(7)
+      })
+    } else {
+      cb({ authenticated: false })
+    }
+  }, 0)
+}*/
+
+function realRequest(email,pass,cb){
+
+  axios.post('localhost:3000', {
+        email: 'email',
+        password: 'pass'
+      })
+      .then(function (response) {
+        cb({
+          authenticated: true,
+          token: response.data.token
+        })
+      })
+      .catch(function (error) {
+        cb({ authenticated: false })
+      });
+
+}
